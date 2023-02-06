20227d82dcfc617efa68fadbd1889ff4ae0545ae
Siwy55p <szuminski.p@gmail.com>
2023-02-06 17:36:10 +0100

    a

---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_Simple_and_Angular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CretateUser(int i)
+        {
+
+            return Ok();
+        }
+    }
+}
